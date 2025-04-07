3b9eb14cc18a894b4158e920caf11270e5edc1cd
yemmi <163083182+yemmi408@users.noreply.github.com>
2025-04-08 00:16:23 +0500

    naked files
    they had no folders

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# breek
+## Breek
+
+This app was created using https://getmocha.com.
+Need help or want to join the community? Join our [Discord](https://discord.gg/shDEGBSe2d).
+
+To run the devserver:
+```
+npm install
+npm run dev
+```
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Instrument Sans', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', 'Helvetica Neue', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
